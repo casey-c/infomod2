8d6f7387835b6cd91c664458142420041d8cbf29
casey <406326-casey@users.noreply.gitlab.com>
2021-02-23 15:48:34 -0500

    Event chance helper

--- src/main/java/InfoMod2/ui/widgets/tooltips/map/EventChanceToolTip.java
+++ src/main/java/InfoMod2/ui/widgets/tooltips/map/EventChanceToolTip.java
@@ -2,6 +2,7 @@ package InfoMod2.ui.widgets.tooltips.map;
 
 import InfoMod2.ui.widgets.tooltips.TitledToolTip;
 import InfoMod2.utils.graphics.ExtraColors;
+import InfoMod2.utils.math.EventChanceHelper;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.megacrit.cardcrawl.core.Settings;
@@ -22,12 +23,7 @@ public class EventChanceToolTip extends TitledToolTip<EventChanceToolTip> {
     private Color getTreasureColor() { return ExtraColors.QUAL_GREEN; }
 
     // TODO: write update functions etc.
-    private String eventText = "63.75%";
-    private String shrineText = "21.25%";
-    private String fightText = "10.00%";
-    private String shopText = "3.00%";
-    private String treasureText = "2.00%";
-
+    EventChanceHelper helper = new EventChanceHelper();
 
     @Override
     protected void renderContent(SpriteBatch sb, float left, float top) {
@@ -37,26 +33,26 @@ public class EventChanceToolTip extends TitledToolTip<EventChanceToolTip> {
 
         final Color eventColor = getEventColor();
         FontHelper.renderFontLeftTopAligned(sb, FontHelper.tipBodyFont, "Event", left * Settings.scale, currY * Settings.scale, eventColor);
-        FontHelper.renderFontLeftTopAligned(sb, FontHelper.tipBodyFont, eventText, detailLeft * Settings.scale, currY * Settings.scale, eventColor);
+        FontHelper.renderFontLeftTopAligned(sb, FontHelper.tipBodyFont, helper.getEventChance(), detailLeft * Settings.scale, currY * Settings.scale, eventColor);
         currY -= SPACING;
 
         final Color shrineColor = getShrineColor();
         FontHelper.renderFontLeftTopAligned(sb, FontHelper.tipBodyFont, "Shrine", left * Settings.scale, currY * Settings.scale, shrineColor);
-        FontHelper.renderFontLeftTopAligned(sb, FontHelper.tipBodyFont, shrineText, detailLeft * Settings.scale, currY * Settings.scale, shrineColor);
+        FontHelper.renderFontLeftTopAligned(sb, FontHelper.tipBodyFont, helper.getShrineChance(), detailLeft * Settings.scale, currY * Settings.scale, shrineColor);
         currY -= SPACING;
 
         final Color fightColor = getFightColor();
         FontHelper.renderFontLeftTopAligned(sb, FontHelper.tipBodyFont, "Fight", left * Settings.scale, currY * Settings.scale, fightColor);
-        FontHelper.renderFontLeftTopAligned(sb, FontHelper.tipBodyFont, fightText, detailLeft * Settings.scale, currY * Settings.scale, fightColor);
+        FontHelper.renderFontLeftTopAligned(sb, FontHelper.tipBodyFont, helper.getFightChance(), detailLeft * Settings.scale, currY * Settings.scale, fightColor);
         currY -= SPACING;
 
         final Color shopColor = getShopColor();
         FontHelper.renderFontLeftTopAligned(sb, FontHelper.tipBodyFont, "Shop", left * Settings.scale, currY * Settings.scale, shopColor);
-        FontHelper.renderFontLeftTopAligned(sb, FontHelper.tipBodyFont, shopText, detailLeft * Settings.scale, currY * Settings.scale, shopColor);
+        FontHelper.renderFontLeftTopAligned(sb, FontHelper.tipBodyFont, helper.getShopChance(), detailLeft * Settings.scale, currY * Settings.scale, shopColor);
         currY -= SPACING;
 
         final Color treasureColor = getTreasureColor();
         FontHelper.renderFontLeftTopAligned(sb, FontHelper.tipBodyFont, "Treasure", left * Settings.scale, currY * Settings.scale, treasureColor);
-        FontHelper.renderFontLeftTopAligned(sb, FontHelper.tipBodyFont, treasureText, detailLeft * Settings.scale, currY * Settings.scale, treasureColor);
+        FontHelper.renderFontLeftTopAligned(sb, FontHelper.tipBodyFont, helper.getTreasureChance(), detailLeft * Settings.scale, currY * Settings.scale, treasureColor);
     }
 }
---
+++ src/main/java/InfoMod2/utils/math/EventChanceHelper.java
@@ -0,0 +1,56 @@
+package InfoMod2.utils.math;
+
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.EventHelper;
+
+import java.util.ArrayList;
+import java.util.Vector;
+
+public class EventChanceHelper {
+    private float prMonster;
+    private float prShop;
+    private float prTreasure;
+    private float prEvent;
+
+    private String event, shrine, monster, shop, treasure;
+
+    // --------------------------------------------------------------------------------
+
+    public String getEventChance() { return event; }
+    public String getShrineChance() { return shrine; }
+    public String getFightChance() { return monster; }
+    public String getShopChance() { return shop; }
+    public String getTreasureChance() { return treasure; }
+
+    private void updateTextCache() {
+        this.event = String.format("%.02f%%", ((prEvent * 0.75f) * 100.0f));
+        this.shrine = String.format("%.02f%%", ((prEvent * 0.25f) * 100.0f));
+        this.monster = String.format("%.02f%%", (prMonster * 100.0f));
+        this.shop = String.format("%.02f%%", (prShop * 100.0f));
+        this.treasure = String.format("%.02f%%", (prTreasure * 100.0f));
+    }
+
+    // --------------------------------------------------------------------------------
+
+    public void update() {
+        // Compute probabilities
+        ArrayList<Float> eventChanceList = AbstractDungeon.loading_post_combat ? EventHelper.getChancesPreRoll() : EventHelper.getChances();
+        Vector<Float> eventChanceVec = new Vector<>(eventChanceList);
+
+        // eventChanceVec: elite (useless), monster, shop, treasure
+        this.prMonster = eventChanceVec.get(1);
+        this.prShop = eventChanceVec.get(2);
+        this.prTreasure = eventChanceVec.get(3);
+        this.prEvent = 1.0f - prMonster - prShop - prTreasure;
+
+        // JUZU BRACELET fix
+        AbstractPlayer player = AbstractDungeon.player;
+        if (player != null && player.hasRelic("Juzu Bracelet")) {
+            this.prEvent += prMonster;
+            this.prMonster = 0.0f;
+        }
+
+        updateTextCache();
+    }
+}

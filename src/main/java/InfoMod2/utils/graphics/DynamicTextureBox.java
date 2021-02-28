cd9bfcea74eee09f9342147f866dbd1e87685002
casey <406326-casey@users.noreply.gitlab.com>
2021-02-28 04:02:49 -0500

    Fixing DynTexBox corners

--- src/main/java/InfoMod2/utils/graphics/DynamicTextureBox.java
+++ src/main/java/InfoMod2/utils/graphics/DynamicTextureBox.java
@@ -16,7 +16,10 @@ public class DynamicTextureBox {
     private Texture TEX_EDGE_BASE;
 
     private int CORNER_SIZE;
-    private int CENTER_OFFSET;
+//    private int CENTER_OFFSET;
+
+    private int CENTER_X;
+    private int CENTER_Y;
 
     private Color outerBevelColor, innerBevelColor, baseColor;
 
@@ -29,7 +32,11 @@ public class DynamicTextureBox {
         this.TEX_EDGE_BASE = new Texture(folder + "edgeBase.png");
 
         this.CORNER_SIZE = TEX_CORNER_BASE.getWidth();
-        this.CENTER_OFFSET = CORNER_SIZE / 2;
+
+//        this.CENTER_OFFSET = CORNER_SIZE / 2;
+        this.CENTER_X = this.CENTER_Y = CORNER_SIZE / 2;
+//        this.CENTER_X = (int)(((float)CORNER_SIZE / 2.0f) * Settings.xScale);
+//        this.CENTER_Y = (int)(((float)CORNER_SIZE / 2.0f) * Settings.yScale);
     }
 
     public DynamicTextureBox withColors(Color outerBevel, Color innerBevel, Color baseColor) {
@@ -40,26 +47,38 @@ public class DynamicTextureBox {
     }
 
     // Rendered in order OUTER BEVEL -> INNER BEVEL -> BASE (on top, last)
-    public void renderCorner(SpriteBatch sb, float left, float bottom, float rot, boolean flipX, boolean flipY) {
+    public void renderCorner(SpriteBatch sb, float left, float bottom, boolean flipX, boolean flipY) {
         sb.setColor(outerBevelColor);
-        sb.draw(TEX_CORNER_OUTER_BEVEL, left * Settings.scale, bottom * Settings.scale, CENTER_OFFSET * Settings.scale, CENTER_OFFSET * Settings.scale, CORNER_SIZE * Settings.scale, CORNER_SIZE * Settings.scale, 1, 1, rot, 0, 0, CORNER_SIZE, CORNER_SIZE, flipX, flipY);
+        sb.draw(TEX_CORNER_OUTER_BEVEL, left * Settings.xScale, bottom * Settings.yScale, CENTER_X, CENTER_Y, CORNER_SIZE * Settings.xScale, CORNER_SIZE * Settings.yScale, 1, 1, 0, 0, 0, CORNER_SIZE, CORNER_SIZE, flipX, flipY);
 
         sb.setColor(innerBevelColor);
-        sb.draw(TEX_CORNER_INNER_BEVEL, left * Settings.scale, bottom * Settings.scale, CENTER_OFFSET * Settings.scale, CENTER_OFFSET * Settings.scale, CORNER_SIZE * Settings.scale, CORNER_SIZE * Settings.scale, 1, 1, rot, 0, 0, CORNER_SIZE, CORNER_SIZE, flipX, flipY);
+        sb.draw(TEX_CORNER_INNER_BEVEL, left * Settings.xScale, bottom * Settings.yScale, CENTER_X, CENTER_Y, CORNER_SIZE * Settings.xScale, CORNER_SIZE * Settings.yScale, 1, 1, 0, 0, 0, CORNER_SIZE, CORNER_SIZE, flipX, flipY);
 
         sb.setColor(baseColor);
-        sb.draw(TEX_CORNER_BASE, left * Settings.scale, bottom * Settings.scale, CENTER_OFFSET * Settings.scale, CENTER_OFFSET * Settings.scale, CORNER_SIZE * Settings.scale, CORNER_SIZE * Settings.scale, 1, 1, rot, 0, 0, CORNER_SIZE, CORNER_SIZE, flipX, flipY);
+        sb.draw(TEX_CORNER_BASE, left * Settings.xScale, bottom * Settings.yScale, CENTER_X, CENTER_Y, CORNER_SIZE * Settings.xScale, CORNER_SIZE * Settings.yScale, 1, 1, 0, 0, 0, CORNER_SIZE, CORNER_SIZE, flipX, flipY);
+    }
+
+    // Required for rotation
+    public void renderCorner(SpriteBatch sb, float left, float bottom, float rot, boolean flipX, boolean flipY, int rotOriginX, int rotOriginY, float horizontalScaling, float verticalScaling) {
+        sb.setColor(outerBevelColor);
+        sb.draw(TEX_CORNER_OUTER_BEVEL, left * horizontalScaling, bottom * Settings.yScale, rotOriginX, rotOriginY, CORNER_SIZE * horizontalScaling, CORNER_SIZE * verticalScaling, 1, 1, rot, 0, 0, CORNER_SIZE, CORNER_SIZE, flipX, flipY);
+
+        sb.setColor(innerBevelColor);
+        sb.draw(TEX_CORNER_INNER_BEVEL, left * Settings.xScale, bottom * Settings.yScale, rotOriginX, rotOriginY, CORNER_SIZE * horizontalScaling, CORNER_SIZE * verticalScaling, 1, 1, rot, 0, 0, CORNER_SIZE, CORNER_SIZE, flipX, flipY);
+
+        sb.setColor(baseColor);
+        sb.draw(TEX_CORNER_BASE, left * Settings.xScale, bottom * Settings.yScale, rotOriginX, rotOriginY, CORNER_SIZE * horizontalScaling, CORNER_SIZE * verticalScaling, 1, 1, rot, 0, 0, CORNER_SIZE, CORNER_SIZE, flipX, flipY);
     }
 
     public void renderEdge(SpriteBatch sb, float left, float bottom, float width, float height, float rot, boolean flipX, boolean flipY) {
         sb.setColor(outerBevelColor);
-        sb.draw(TEX_EDGE_OUTER_BEVEL, left * Settings.scale, bottom * Settings.scale, CENTER_OFFSET * Settings.scale, CENTER_OFFSET * Settings.scale, width * Settings.scale, height * Settings.scale, 1, 1, rot, 0, 0, CORNER_SIZE, CORNER_SIZE, flipX, flipY);
+        sb.draw(TEX_EDGE_OUTER_BEVEL, left * Settings.xScale, bottom * Settings.yScale, CENTER_X, CENTER_Y, width * Settings.xScale, height * Settings.yScale, 1, 1, rot, 0, 0, CORNER_SIZE, CORNER_SIZE, flipX, flipY);
 
         sb.setColor(innerBevelColor);
-        sb.draw(TEX_EDGE_INNER_BEVEL, left * Settings.scale, bottom * Settings.scale, CENTER_OFFSET * Settings.scale, CENTER_OFFSET * Settings.scale, width * Settings.scale, height * Settings.scale, 1, 1, rot, 0, 0, CORNER_SIZE, CORNER_SIZE, flipX, flipY);
+        sb.draw(TEX_EDGE_INNER_BEVEL, left * Settings.xScale, bottom * Settings.yScale, CENTER_X, CENTER_Y, width * Settings.xScale, height * Settings.yScale, 1, 1, rot, 0, 0, CORNER_SIZE, CORNER_SIZE, flipX, flipY);
 
         sb.setColor(baseColor);
-        sb.draw(TEX_EDGE_BASE, left * Settings.scale, bottom * Settings.scale, CENTER_OFFSET * Settings.scale, CENTER_OFFSET * Settings.scale, width * Settings.scale, height * Settings.scale, 1, 1, rot, 0, 0, CORNER_SIZE, CORNER_SIZE, flipX, flipY);
+        sb.draw(TEX_EDGE_BASE, left * Settings.xScale, bottom * Settings.yScale, CENTER_X, CENTER_Y, width * Settings.xScale, height * Settings.yScale, 1, 1, rot, 0, 0, CORNER_SIZE, CORNER_SIZE, flipX, flipY);
     }
 
     // TODO: probably should cache some of this because it doesn't really need to do all these calculations per frame
@@ -73,17 +92,18 @@ public class DynamicTextureBox {
         float edgeWidth = innerRight - innerLeft;
         float edgeHeight = innerTop - innerBottom;
 
-        renderCorner(sb, left, bottom, 0, false, true); // bottom left
-        renderCorner(sb, left, innerTop, 0, false, false); // top left
-        renderCorner(sb, innerRight, innerTop, 0, true, false); // top right
-        renderCorner(sb, innerRight, bottom, -90, true, false); // bottom right
+        renderCorner(sb, left, bottom, false, true); // bottom left
+        renderCorner(sb, left, innerTop, false, false); // top left
+        renderCorner(sb, innerRight, innerTop, true, false); // top right
+        renderCorner(sb, innerRight, bottom, true, true); // bottom right
+//        renderCorner(sb, innerRight, bottom, -90, true, false, CENTER_X, CENTER_Y, Settings.yScale, Settings.xScale); // bottom right
 
-        renderEdge(sb, innerLeft, bottom, edgeWidth, CORNER_SIZE, 0, false, true); // bottom edge
-        renderEdge(sb, innerLeft, innerTop, edgeWidth, CORNER_SIZE, 0, false, false); // top edge
-        renderEdge(sb, left, innerBottom, edgeHeight, CORNER_SIZE, 90, false, false); // left edge
-        renderEdge(sb, innerRight, innerBottom + edgeHeight - CORNER_SIZE, edgeHeight, CORNER_SIZE, -90, false, false); // right edge
+//        renderEdge(sb, innerLeft, bottom, edgeWidth, CORNER_SIZE, 0, false, true); // bottom edge
+//        renderEdge(sb, innerLeft, innerTop, edgeWidth, CORNER_SIZE, 0, false, false); // top edge
+//        renderEdge(sb, left, innerBottom, edgeHeight, CORNER_SIZE, 90, false, false); // left edge
+//        renderEdge(sb, innerRight, innerBottom + edgeHeight - CORNER_SIZE, edgeHeight, CORNER_SIZE, -90, false, false); // right edge
 
         sb.setColor(baseColor);
-        sb.draw(ImageMaster.WHITE_SQUARE_IMG, innerLeft * Settings.scale, innerBottom * Settings.scale, edgeWidth * Settings.scale, edgeHeight * Settings.scale);
+        sb.draw(ImageMaster.WHITE_SQUARE_IMG, innerLeft * Settings.xScale, innerBottom * Settings.yScale, edgeWidth * Settings.xScale, edgeHeight * Settings.yScale);
     }
 }

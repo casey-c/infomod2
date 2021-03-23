ae2d3191dc3959e269b26f03dd688ebd94e83dc7
casey <406326-casey@users.noreply.gitlab.com>
2021-03-23 17:41:19 -0400

    Bugfix: prevent tips going off right side of screen

--- src/main/java/InfoMod2/ui/tips/EventDetailTip.java
+++ src/main/java/InfoMod2/ui/tips/EventDetailTip.java
@@ -219,24 +219,31 @@ public class EventDetailTip extends AbstractWidget<EventDetailTip> {
 
         // Compute the vertical offset (to make sure tips don't run off the bottom of the screen)
         float verticalOffset = 0.0f;
-        if (realBottom < 0.0f)
-            verticalOffset = 0.0f - realBottom + 10.0f;
-        else if (realBottom < 10.0f)
+        if (realBottom < 10.0f)
             verticalOffset = 10.0f - realBottom;
 
-        float h = top - realBottom;
+        // Figure out the right side of the tool tip
+        float realRight = left + tooltipWidth;
+        System.out.println("Real right is: " + realRight);
 
-        textureBox.render(sb, left, realBottom + verticalOffset, w, h);
+        // Compute the horizontal offset (to make sure tips don't run off the right side of the screen)
+        float horizontalOffset = 0.0f;
+        final float horizontalRefPoint = 1840.0f;
+        if (realRight > horizontalRefPoint)
+            horizontalOffset = horizontalRefPoint - realRight;
+
+        float h = top - realBottom;
+        textureBox.render(sb, left + horizontalOffset, realBottom + verticalOffset, w, h);
 
         // Render the name of the event
-        titleLabel.anchoredAt(textLeft, titleBottom + verticalOffset, AnchorPosition.LEFT_BOTTOM);
+        titleLabel.anchoredAt(textLeft + horizontalOffset, titleBottom + verticalOffset, AnchorPosition.LEFT_BOTTOM);
         titleLabel.render(sb);
 
         // Render all the requirements for this event
         float currX = textLeft;
 
         for (SmartLabel label : reqLabels) {
-            label.anchoredAt(currX, reqBottom + verticalOffset, AnchorPosition.LEFT_BOTTOM);
+            label.anchoredAt(currX + horizontalOffset, reqBottom + verticalOffset, AnchorPosition.LEFT_BOTTOM);
             label.render(sb);
             currX += label.getPreferredContentWidth() + REQ_LABEL_SPACING;
         }
@@ -244,13 +251,13 @@ public class EventDetailTip extends AbstractWidget<EventDetailTip> {
         // Render the divider line
         final float DIVIDER_OFFSET = 23.0f;
         sb.setColor(ExtraColors.DIVIDER_COLOR);
-        sb.draw(ImageMaster.WHITE_SQUARE_IMG, (left + DIVIDER_OFFSET) * Settings.xScale, (dividerBottom + verticalOffset) * Settings.yScale, (w - (2 * DIVIDER_OFFSET)) * Settings.xScale, 3.0f);
+        sb.draw(ImageMaster.WHITE_SQUARE_IMG, (left + horizontalOffset + DIVIDER_OFFSET) * Settings.xScale, (dividerBottom + verticalOffset) * Settings.yScale, (w - (2 * DIVIDER_OFFSET)) * Settings.xScale, 3.0f);
 
         // Choice cards
         float currY = dividerBottom - 22.0f;
 
         for (EventChoiceCard choiceCard : choiceCards) {
-            choiceCard.anchoredAt(textLeft, currY + verticalOffset, AnchorPosition.LEFT_TOP);
+            choiceCard.anchoredAt(textLeft + horizontalOffset, currY + verticalOffset, AnchorPosition.LEFT_TOP);
             choiceCard.render(sb);
 
             currY -= (choiceCard.getPreferredContentHeight() + CHOICE_CARD_SPACING);
@@ -259,7 +266,7 @@ public class EventDetailTip extends AbstractWidget<EventDetailTip> {
         // Render the notes label
         if (notesLabel != null) {
             currY -= CHOICES_NOTES_GAP;
-            notesLabel.anchoredAt(textLeft, currY + verticalOffset, AnchorPosition.LEFT_TOP);
+            notesLabel.anchoredAt(textLeft + horizontalOffset, currY + verticalOffset, AnchorPosition.LEFT_TOP);
             notesLabel.render(sb);
         }
     }
